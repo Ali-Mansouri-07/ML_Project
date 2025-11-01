ae0a37b65775c9c400c2bde66ac10316146f5681
Ali Mansouri <mansouriali215@gmail.com>
2025-11-01 14:55:20 +0100

    new branch 1/2

--- Project.py
+++ Project.py
@@ -2,3 +2,7 @@ print("Hello Word world hello !")
 print("Hello Word world hello !")
 print("Hello Word world hello !")
 print("Hello Word world hello !")
+
+x=5
+y=6
+print("Hello Word world hello !")
